eff60dd4042927db9b38cbd8d91f61871b242254
WhitexNinja <davipedrosilvaalves@gmail.com>
2025-04-02 12:41:08 -0300

    .

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
+import { Button, Avatar } from 'react-native-elements';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.string}>Eliel Peixoto Teófilo de Jesus</Text>
       <StatusBar style="auto" />
+
+      <Avatar
+        rounded
+        source={{
+          uri:
+            ''
+        }}/>
+      <Text>Email</Text>
+      <TextInput></TextInput>
+      <Text>Senha</Text>
+      <TextInput></TextInput>
+      <Button type='solid'>Logar</Button>
+      <Button type='solid'>Cadastre-se</Button>
+      <Text>Esqueceu a senha?</Text>
     </View>
   );
 }
